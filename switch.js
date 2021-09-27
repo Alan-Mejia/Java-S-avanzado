b4758bad0a6154f7bb0150797c4644c3304261b1
Alan Mejia <mejiamartinezalan@gmail.com>
2021-09-26 19:08:32 -0500

    Ciclo switch en JS

---
+++ switch.js
@@ -0,0 +1,20 @@
+const edad = parseInt(prompt("Ingresa tu edad"));
+let mensaje;
+
+switch(edad){
+    case 18:
+        mensaje="Acaba de cumplir la mayoria de edad";
+        break;
+    case 25:
+        mensaje="Ya eres adulto"
+        break;
+    case 70:
+        mensaje="Ya eres de la tercera edad";
+        break;
+    default:
+        mensaje="Fuera del rango de edad";
+        break;
+}
+
+document.write(`<p>${mensaje}</p>`);
+
