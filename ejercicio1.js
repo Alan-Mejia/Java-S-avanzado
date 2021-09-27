5e0a3de80fbf21dba32af3cdd529f2b326bc9d8a
Alan Mejia <mejiamartinezalan@gmail.com>
2021-09-26 19:14:08 -0500

    Imprimiendo desde el JS

---
+++ ejercicio1.js
@@ -0,0 +1,10 @@
+let anioDeNac= parseInt(prompt("Ingresa tu año de nacimiento"));
+
+const calcula = `<h1>Tu edad es ${2021-anioDeNac}</h1>`; 
+document.write(calcula);
+
+//2021-2002    19  9  1
+if(calcula%2==0)
+    document.write("Es un numero par")
+else
+    document.write("Es un numero Impar")
